f55cfc0369bd76d875a045862c760e9fd4715da5
Valter <valterh4813@gmail.com>
2025-10-31 21:47:06 -0300

    quase finalizado

--- codigo-hamming.py
+++ codigo-hamming.py
@@ -9,96 +9,92 @@ print(f'codigo recebido {lista_de_digitos}')
 while (2**k) < (k+len(lista_de_digitos)+1):
     k += 1
     
-bits_paridade_original = []
 
-bits_paridade_recebida = []
 
 
 print(f'Serão precisos {k} bits de verificação')
 
-if(len(lista_de_digitos) == 4 or len(lista_de_digitos) == 5):
+if(len(lista_de_digitos) == 4):
     if( ((lista_de_digitos[0] + lista_de_digitos[1] + lista_de_digitos[3])%2) != 0):
         lista_de_digitos.insert(0, 1)
-        bits_paridade_original.append(1)
     else:
         lista_de_digitos.insert(0, 0)
-        bits_paridade_original.append(0)
 
     if( ((lista_de_digitos[1] + lista_de_digitos[3] + lista_de_digitos[4])%2) != 0):
         lista_de_digitos.insert(1, 1)
-        bits_paridade_original.append(1)
     else:
         lista_de_digitos.insert(1, 0)
-        bits_paridade_original.append(0)
 
     if( ((lista_de_digitos[3] + lista_de_digitos[4] + lista_de_digitos[5])%2) != 0):
         lista_de_digitos.insert(3, 1)
-        bits_paridade_original.append(1)
     else:
         lista_de_digitos.insert(3, 0)
-        bits_paridade_original.append(0)
+elif(len(lista_de_digitos) == 5):
+    if( ((lista_de_digitos[0] + lista_de_digitos[1] + lista_de_digitos[3])%2) != 0):
+        lista_de_digitos.insert(0, 1)
+    else:
+        lista_de_digitos.insert(0, 0)
 
+    if( ((lista_de_digitos[1] + lista_de_digitos[3] + lista_de_digitos[4])%2) != 0):
+        lista_de_digitos.insert(1, 1)
+    else:
+        lista_de_digitos.insert(1, 0)
+
+    if( ((lista_de_digitos[3] + lista_de_digitos[4] + lista_de_digitos[5])%2) != 0):
+        lista_de_digitos.insert(3, 1)
+    else:
+        lista_de_digitos.insert(3, 0)
+
+    if( ((lista_de_digitos[7])%2) != 0):
+        lista_de_digitos.insert(7, 1)
+    else:
+        lista_de_digitos.insert(7, 0)
 
 elif(len(lista_de_digitos) == 6):
     if( ((lista_de_digitos[0] + lista_de_digitos[1] + lista_de_digitos[3] + lista_de_digitos[4])%2) != 0):
         lista_de_digitos.insert(0, 1)
-        bits_paridade_original.append(1)
     else:
         lista_de_digitos.insert(0, 0)
-        bits_paridade_original.append(0)
 
     if( ((lista_de_digitos[1] + lista_de_digitos[3] + lista_de_digitos[4] + lista_de_digitos[-1])%2) != 0):
         lista_de_digitos.insert(1, 1)
-        bits_paridade_original.append(1)
     else:
         lista_de_digitos.insert(1, 0)
-        bits_paridade_original.append(0) 
+
 
 
     if( ((lista_de_digitos[3] + lista_de_digitos[4] + lista_de_digitos[5])%2) != 0):
         lista_de_digitos.insert(3, 1)
-        bits_paridade_original.append(1)
     else:
         lista_de_digitos.insert(3, 0)
-        bits_paridade_original.append(0)
 
 
     if( ((lista_de_digitos[7] + lista_de_digitos[8])%2) != 0):
         lista_de_digitos.insert(7, 1)
-        bits_paridade_original.append(1)
     else:
         lista_de_digitos.insert(7, 0)
-        bits_paridade_original.append(0)
+
 
 
 elif(len(lista_de_digitos) == 7):
     if( ((lista_de_digitos[0] + lista_de_digitos[1] + lista_de_digitos[3] + lista_de_digitos[4] + lista_de_digitos[-1])%2) != 0):
         lista_de_digitos.insert(0, 1)
-        bits_paridade_original.append(1)
     else:
         lista_de_digitos.insert(0, 0)
-        bits_paridade_original.append(0)
-
     if( ((lista_de_digitos[1] + lista_de_digitos[3] + lista_de_digitos[4] + lista_de_digitos[-2] + lista_de_digitos[-1])%2) != 0):
         lista_de_digitos.insert(1, 1)
-        bits_paridade_original.append(1)
     else:
         lista_de_digitos.insert(1, 0)
-        bits_paridade_original.append(0)
 
     if( ((lista_de_digitos[3] + lista_de_digitos[4] + lista_de_digitos[5])%2) != 0):
         lista_de_digitos.insert(3, 1)
-        bits_paridade_original.append(1)
     else:
         lista_de_digitos.insert(3, 0)
-        bits_paridade_original.append(0)
 
     if( ((lista_de_digitos[-3] + lista_de_digitos[-2] + lista_de_digitos[-1])%2) != 0):
         lista_de_digitos.insert(7, 1)
-        bits_paridade_original.append(1)
     else:
         lista_de_digitos.insert(7, 0)
-        bits_paridade_original.append(0)
 
 
 ###CORREÇÃO DO CÓDIGO
@@ -109,35 +105,45 @@ codigo_recebido = input("Digite o código de bits que você recebeu para corrigi
 
 lista_bits_recebida = [int(digito) for digito in codigo_recebido]
 
+posicao = 0
 
-if(k == 3):
-    bits_paridade_recebida.append(lista_de_digitos[0])
-    bits_paridade_recebida.append(lista_bits_recebida[1])
-    bits_paridade_recebida.append(lista_bits_recebida[3])
+if(len(lista_de_digitos) == 7):
+    if( ((lista_bits_recebida[0] + lista_bits_recebida[2] + lista_bits_recebida[4] + lista_bits_recebida[6] + lista_bits_recebida[8])%2) != 0):
+        posicao += 1
 
-if(k == 4): 
-    bits_paridade_recebida.append(lista_bits_recebida[0])
-    bits_paridade_recebida.append(lista_bits_recebida[1])
-    bits_paridade_recebida.append(lista_bits_recebida[3])
-    bits_paridade_recebida.append(lista_bits_recebida[7])
-    
-posicao_bit_errado = 0
-    
-for i in range(len(bits_paridade_original)):
-    if(bits_paridade_original[i] == bits_paridade_original[i]):
-        posicao_bit_errado += 0 * (2**i)
-    else:
-        posicao_bit_errado += 1 * (2**i)
-        
+    if( ((lista_bits_recebida[1] + lista_bits_recebida[2] + lista_bits_recebida[5] + lista_bits_recebida[6])%2) != 0):
+        posicao += 2
 
-print(bits_paridade_original)
-print(bits_paridade_recebida)
+    if( ((lista_bits_recebida[3] + lista_bits_recebida[4] + lista_bits_recebida[5]+ lista_bits_recebida[6])%2) != 0):
+        posicao += 4
+if(len(lista_de_digitos) == 9):
+    if( ((lista_bits_recebida[0] + lista_bits_recebida[2] + lista_bits_recebida[4] + lista_bits_recebida[6])%2) != 0):
+        posicao += 1
 
-print(f'A posição do bit errado é {posicao_bit_errado}')
+    if( ((lista_bits_recebida[1] + lista_bits_recebida[2] + lista_bits_recebida[5] + lista_bits_recebida[6])%2) != 0):
+        posicao += 2
 
-if(codigo_recebido[posicao_bit_errado] == 1):
-    codigo_recebido[posicao_bit_errado] = 0
-elif(codigo_recebido[posicao_bit_errado] == 0):
-    codigo_recebido[posicao_bit_errado] = 1
+    if( ((lista_bits_recebida[3] + lista_bits_recebida[4] + lista_bits_recebida[5]+ lista_bits_recebida[6])%2) != 0):
+        posicao += 4
+    if( ((lista_bits_recebida[7] + lista_bits_recebida[8])%2) != 0):
+        posicao += 8
+if(len(lista_de_digitos) == 10):
+    if( ((lista_bits_recebida[0] + lista_bits_recebida[2] + lista_bits_recebida[4] + lista_bits_recebida[6] + lista_bits_recebida[8])%2) != 0):
+        posicao += 1
 
-print(f'Esse é o codigo recebido corrigido: {codigo_recebido}')
+    if( ((lista_bits_recebida[1] + lista_bits_recebida[2] + lista_bits_recebida[5] + lista_bits_recebida[6] + lista_bits_recebida[9])%2) != 0):
+        posicao += 2
+
+    if( ((lista_bits_recebida[3] + lista_bits_recebida[4] + lista_bits_recebida[5]+ lista_bits_recebida[6])%2) != 0):
+        posicao += 4
+    if( ((lista_bits_recebida[7] + lista_bits_recebida[8] + lista_bits_recebida[9])%2) != 0):
+        posicao += 8
+
+
+if(lista_bits_recebida[posicao-1] == 1):
+    lista_bits_recebida[posicao-1] = 0
+else:
+    lista_bits_recebida[posicao-1] = 1
+
+
+print(f'Esse é o codigo recebido corrigido: {lista_bits_recebida}')
